0337d92a0bb5b56cec6901372d1ca127af0591e0
jesse keane <me@jessekeane.me>
2014-04-23 17:30:19 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,94 @@
+var path = require('path')
+  , fs = require('fs')
+
+var through = require('through')
+
+module.exports = json_stream
+
+function json_stream(dir, _options, _extension) {
+  var extension = _extension || '.json'
+    , options = _options || {}
+    , stream = through()
+    , read
+
+  if(options.limit === 0) return stream.queue(null)
+
+  if(options.limit === -1) options.limit = 0
+
+  fs.readdir(dir, parse_files)
+
+  return stream
+
+  function parse_files(err, files) {
+    files = files.sort().slice(0, options.limit)
+    if(options.start || options.end) files = files.filter(filter_start_end)
+
+    if(!files.length) return stream.queue(null)
+    if(options.reverse) files = files.reverse()
+
+    stream_file(files.shift())
+
+    function stream_file(filename) {
+      if(path.extname(filename) !== '.json') return next()
+
+      if(options.values === false) {
+        return fs.stat(path.join(dir, filename), stream_key)
+      }
+
+      fs.readFile(path.join(dir, filename), stream_key_value)
+
+      function stream_key(err, stats) {
+        if(err || !stats.isFile()) return next()
+
+        stream.queue(just_name(filename))
+
+        next()
+      }
+
+      function stream_key_value(err, data) {
+        if(err) return next()
+
+        var result
+          , value
+
+        try{
+          value = JSON.stringify(JSON.parse('' + data))
+        } catch(e) {
+          next()
+        }
+
+        if(options.keys === false) {
+          result = value
+        } else {
+          result = {
+              key: just_name(filename)
+            , value: value
+          }
+        }
+
+        stream.queue(result)
+
+        next()
+      }
+    }
+
+    function next() {
+      if(!files.length) return stream.queue(null)
+
+      stream_file(files.shift())
+    }
+  }
+
+  function filter_start_end(el) {
+    var compare = path.basename(el, extension)
+
+    if(options.start && options.start > compare) return false
+    if(options.end && options.end < compare) return false
+
+    return true
+  }
+}
+
+function just_name(filename) {
+  return path.basename(filename, path.extname(filename))
+}
---
+++ test/test.js
@@ -0,0 +1,202 @@
+var test = require('tape')
+
+var read_stream = require('../')
+
+test('streams keys and values of everything by default', function(t) {
+  t.plan(1)
+
+  var stream = read_stream(__dirname + '/json-dir')
+    , results = []
+    , expected
+
+  expected = [
+      {key: 'a', value: '{"lol":true}'}
+    , {key: 'b', value: '{"lol":true}'}
+    , {key: 'bar', value: '{"lol":true}'}
+    , {key: 'c', value: '{"lol":true}'}
+    , {key: 'd', value: '{"lol":true}'}
+    , {key: 'e', value: '{"lol":true}'}
+    , {key: 'f', value: '{"lol":true}'}
+    , {key: 'h', value: '{"lol":true}'}
+  ]
+
+  stream.on('data', function(data) {
+    results.push(data)
+  })
+
+  stream.on('end', function() {
+    t.deepEqual(results, expected)
+  })
+})
+
+test('streams keys and values in reverse if specified', function(t) {
+  t.plan(1)
+
+  var stream = read_stream(__dirname + '/json-dir', {reverse: true})
+    , results = []
+    , expected
+
+  expected = [
+      {key: 'a', value: '{"lol":true}'}
+    , {key: 'b', value: '{"lol":true}'}
+    , {key: 'bar', value: '{"lol":true}'}
+    , {key: 'c', value: '{"lol":true}'}
+    , {key: 'd', value: '{"lol":true}'}
+    , {key: 'e', value: '{"lol":true}'}
+    , {key: 'f', value: '{"lol":true}'}
+    , {key: 'h', value: '{"lol":true}'}
+  ].reverse()
+
+  stream.on('data', function(data) {
+    results.push(data)
+  })
+
+  stream.on('end', function() {
+    t.deepEqual(results, expected)
+  })
+})
+
+test('streams just keys if specified', function(t) {
+  t.plan(1)
+
+  var stream = read_stream(__dirname + '/json-dir', {values: false})
+    , results = []
+    , expected
+
+  expected = ['a', 'b', 'bar', 'c', 'd', 'e', 'f', 'h']
+
+  stream.on('data', function(data) {
+    results.push(data)
+  })
+
+  stream.on('end', function() {
+    t.deepEqual(results, expected)
+  })
+})
+
+test('streams just values if specified', function(t) {
+  t.plan(1)
+
+  var stream = read_stream(__dirname + '/json-dir', {keys: false})
+    , results = []
+    , expected
+
+  expected = [
+      '{"lol":true}'
+    , '{"lol":true}'
+    , '{"lol":true}'
+    , '{"lol":true}'
+    , '{"lol":true}'
+    , '{"lol":true}'
+    , '{"lol":true}'
+    , '{"lol":true}'
+  ]
+
+  stream.on('data', function(data) {
+    results.push(data)
+  })
+
+  stream.on('end', function() {
+    t.deepEqual(results, expected)
+  })
+})
+
+test('respects start value', function(t) {
+  t.plan(1)
+
+  var stream = read_stream(__dirname + '/json-dir', {start: 'bar'})
+    , results = []
+    , expected
+
+  expected = [
+      {key: 'bar', value: '{"lol":true}'}
+    , {key: 'c', value: '{"lol":true}'}
+    , {key: 'd', value: '{"lol":true}'}
+    , {key: 'e', value: '{"lol":true}'}
+    , {key: 'f', value: '{"lol":true}'}
+    , {key: 'h', value: '{"lol":true}'}
+  ]
+
+  stream.on('data', function(data) {
+    results.push(data)
+  })
+
+  stream.on('end', function() {
+    t.deepEqual(results, expected)
+  })
+})
+
+test('respects start value even if no key matches', function(t) {
+  t.plan(1)
+
+  var stream = read_stream(__dirname + '/json-dir', {start: 'ba'})
+    , results = []
+    , expected
+
+  expected = [
+      {key: 'bar', value: '{"lol":true}'}
+    , {key: 'c', value: '{"lol":true}'}
+    , {key: 'd', value: '{"lol":true}'}
+    , {key: 'e', value: '{"lol":true}'}
+    , {key: 'f', value: '{"lol":true}'}
+    , {key: 'h', value: '{"lol":true}'}
+  ]
+
+  stream.on('data', function(data) {
+    results.push(data)
+  })
+
+  stream.on('end', function() {
+    t.deepEqual(results, expected)
+  })
+})
+
+test('respects end value', function(t) {
+  t.plan(1)
+
+  var stream = read_stream(__dirname + '/json-dir', {end: 'd'})
+    , results = []
+    , expected
+
+  expected = [
+      {key: 'a', value: '{"lol":true}'}
+    , {key: 'b', value: '{"lol":true}'}
+    , {key: 'bar', value: '{"lol":true}'}
+    , {key: 'c', value: '{"lol":true}'}
+    , {key: 'd', value: '{"lol":true}'}
+  ]
+
+  stream.on('data', function(data) {
+    results.push(data)
+  })
+
+  stream.on('end', function() {
+    t.deepEqual(results, expected)
+  })
+})
+
+test('respects end value even if no key matches', function(t) {
+  t.plan(1)
+
+  var stream = read_stream(__dirname + '/json-dir', {end: 'g'})
+    , results = []
+    , expected
+
+  expected = [
+      {key: 'a', value: '{"lol":true}'}
+    , {key: 'b', value: '{"lol":true}'}
+    , {key: 'bar', value: '{"lol":true}'}
+    , {key: 'c', value: '{"lol":true}'}
+    , {key: 'd', value: '{"lol":true}'}
+    , {key: 'e', value: '{"lol":true}'}
+    , {key: 'f', value: '{"lol":true}'}
+  ]
+
+  stream.on('data', function(data) {
+    results.push(data)
+  })
+
+  stream.on('end', function() {
+    t.deepEqual(results, expected)
+  })
+})
